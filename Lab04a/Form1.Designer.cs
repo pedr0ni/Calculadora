fc1be8beeda4b2c52902f3cb9f486e94365d807c
Matheus Pedroni <pedroni.dev@gmail.com>
2018-04-17 01:24:40 -0300

    first commit

---
+++ Lab04a/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Lab04a
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_n1 = new System.Windows.Forms.TextBox();
+            this.txt_n2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_soma = new System.Windows.Forms.Button();
+            this.btn_sub = new System.Windows.Forms.Button();
+            this.btn_multi = new System.Windows.Forms.Button();
+            this.btn_div = new System.Windows.Forms.Button();
+            this.txt_result = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.btn_sair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite o primeiro número:";
+            // 
+            // txt_n1
+            // 
+            this.txt_n1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_n1.Location = new System.Drawing.Point(292, 63);
+            this.txt_n1.Name = "txt_n1";
+            this.txt_n1.Size = new System.Drawing.Size(67, 26);
+            this.txt_n1.TabIndex = 1;
+            // 
+            // txt_n2
+            // 
+            this.txt_n2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_n2.Location = new System.Drawing.Point(292, 96);
+            this.txt_n2.Name = "txt_n2";
+            this.txt_n2.Size = new System.Drawing.Size(67, 26);
+            this.txt_n2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(215, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Digite o segundo número:";
+            // 
+            // btn_soma
+            // 
+            this.btn_soma.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_soma.Location = new System.Drawing.Point(12, 181);
+            this.btn_soma.Name = "btn_soma";
+            this.btn_soma.Size = new System.Drawing.Size(116, 41);
+            this.btn_soma.TabIndex = 4;
+            this.btn_soma.Text = "SOMA";
+            this.btn_soma.UseVisualStyleBackColor = true;
+            this.btn_soma.Click += new System.EventHandler(this.btn_soma_Click);
+            // 
+            // btn_sub
+            // 
+            this.btn_sub.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_sub.Location = new System.Drawing.Point(139, 181);
+            this.btn_sub.Name = "btn_sub";
+            this.btn_sub.Size = new System.Drawing.Size(140, 41);
+            this.btn_sub.TabIndex = 5;
+            this.btn_sub.Text = "SUBTRAÇÃO";
+            this.btn_sub.UseVisualStyleBackColor = true;
+            this.btn_sub.Click += new System.EventHandler(this.btn_sub_Click);
+            // 
+            // btn_multi
+            // 
+            this.btn_multi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_multi.Location = new System.Drawing.Point(285, 181);
+            this.btn_multi.Name = "btn_multi";
+            this.btn_multi.Size = new System.Drawing.Size(163, 41);
+            this.btn_multi.TabIndex = 6;
+            this.btn_multi.Text = "MULTIPLICAÇÃO";
+            this.btn_multi.UseVisualStyleBackColor = true;
+            this.btn_multi.Click += new System.EventHandler(this.btn_multi_Click);
+            // 
+            // btn_div
+            // 
+            this.btn_div.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_div.Location = new System.Drawing.Point(459, 181);
+            this.btn_div.Name = "btn_div";
+            this.btn_div.Size = new System.Drawing.Size(131, 41);
+            this.btn_div.TabIndex = 7;
+            this.btn_div.Text = "DIVISÃO";
+            this.btn_div.UseVisualStyleBackColor = true;
+            this.btn_div.Click += new System.EventHandler(this.btn_div_Click);
+            // 
+            // txt_result
+            // 
+            this.txt_result.BackColor = System.Drawing.Color.DodgerBlue;
+            this.txt_result.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_result.ForeColor = System.Drawing.SystemColors.Window;
+            this.txt_result.Location = new System.Drawing.Point(192, 286);
+            this.txt_result.Name = "txt_result";
+            this.txt_result.ReadOnly = true;
+            this.txt_result.Size = new System.Drawing.Size(87, 26);
+            this.txt_result.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(49, 289);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Resultado:";
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(352, 357);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(116, 41);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "LIMPAR";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // btn_sair
+            // 
+            this.btn_sair.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_sair.Location = new System.Drawing.Point(474, 357);
+            this.btn_sair.Name = "btn_sair";
+            this.btn_sair.Size = new System.Drawing.Size(116, 41);
+            this.btn_sair.TabIndex = 11;
+            this.btn_sair.Text = "SAIR";
+            this.btn_sair.UseVisualStyleBackColor = true;
+            this.btn_sair.Click += new System.EventHandler(this.btn_sair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(602, 410);
+            this.Controls.Add(this.btn_sair);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.txt_result);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_div);
+            this.Controls.Add(this.btn_multi);
+            this.Controls.Add(this.btn_sub);
+            this.Controls.Add(this.btn_soma);
+            this.Controls.Add(this.txt_n2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_n1);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Lab04a - Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_n1;
+        private System.Windows.Forms.TextBox txt_n2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_soma;
+        private System.Windows.Forms.Button btn_sub;
+        private System.Windows.Forms.Button btn_multi;
+        private System.Windows.Forms.Button btn_div;
+        private System.Windows.Forms.TextBox txt_result;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button btn_sair;
+    }
+}
+
---
+++ Lab04a/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab04a
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_sair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private Boolean isValido()
+        {
+            try
+            {
+                double.Parse(txt_n1.Text);
+                double.Parse(txt_n2.Text);
+
+                return true; //Se conseguir dar parse nos 2 valores retorna verdadeiro
+            } catch {
+                MessageBox.Show("Digite apenas números !");
+                txt_result.BackColor = Color.Red;
+                return false; //Se houver algum problema mostra a mensagem muda o BackColor e retorna falso
+            }
+        }
+
+        private void btn_soma_Click(object sender, EventArgs e)
+        {
+            if (!isValido()) return; //Se não for válido o código abaixo não é executado
+
+            double resultado = double.Parse(txt_n1.Text) + double.Parse(txt_n2.Text);
+            txt_result.Text = resultado.ToString();
+            txt_result.BackColor = Color.Green;
+        }
+
+        private void btn_sub_Click(object sender, EventArgs e)
+        {
+            if (!isValido()) return;
+
+            double resultado = double.Parse(txt_n1.Text) - double.Parse(txt_n2.Text);
+            txt_result.Text = resultado.ToString();
+            txt_result.BackColor = Color.Gray;
+        }
+
+        private void btn_multi_Click(object sender, EventArgs e)
+        {
+            if (!isValido()) return;
+
+            double resultado = double.Parse(txt_n1.Text) * double.Parse(txt_n2.Text);
+            txt_result.Text = resultado.ToString();
+            txt_result.BackColor = Color.Blue;
+
+        }
+
+        private void btn_div_Click(object sender, EventArgs e)
+        {
+            if (!isValido()) return;
+            if (double.Parse(txt_n2.Text) == 0)
+            {
+                txt_result.Text = "erro:div";
+                txt_result.BackColor = Color.Red;
+                return;
+            }
+
+            double resultado = double.Parse(txt_n1.Text) / double.Parse(txt_n2.Text);
+            txt_result.Text = resultado.ToString();
+            txt_result.BackColor = Color.Red;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            txt_n1.Clear();
+            txt_n2.Clear();
+            txt_result.Clear();
+            txt_result.BackColor = Color.DodgerBlue;
+        }
+    }
+}
